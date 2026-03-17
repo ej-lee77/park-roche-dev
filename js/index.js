4999f3a8375f98616dbca43a22d226b07d01acda
ej-lee77 <eunji0.0777@gmail.com>
2026-03-17 15:53:15 +0900

    index css까지

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 //main-slider swiper
-let swiper = new Swiper(".main-slider-wrap", {
+let mainSliderSwiper = new Swiper(".main-slider-wrap", {
     loop: true,
     autoplay: {
         delay: 2000,
     },
     speed: 1200,
     effect: "fade",
+});
+
+//special-offers swiper
+let specialOfferSwiper = new Swiper(".special-offer-list-wrap", {
+    slidesPerView: 5,
+    slidesPerGroup: 5,
+    spaceBetween: 20,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".specialof-button-next",
+        prevEl: ".specialof-button-prev",
+    },
 });
