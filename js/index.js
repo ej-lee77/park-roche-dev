8a3decc710a05df075135fcc3dd4c68ad7ae4190
id <email>
2026-03-19 15:05:54 +0900

    서브메뉴, header-js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,37 @@ let specialOfferSwiper = new Swiper(".special-offer-list-wrap", {
         prevEl: ".specialof-button-prev",
     },
 });
+
+// header
+let header = document.querySelector("header");
+let headerHeight = header.offsetHeight;
+let mainSliderHeight = document.querySelector(".main-slider-wrap").offsetHeight;
+let topBtn = document.querySelector('.top-btn');
+let reserveWrap = document.querySelector(".reserve-wrap");
+
+// 스크롤에 따라서 보여주기 숨기기
+document.addEventListener("scroll", ()=>{
+    let sPos = document.documentElement.scrollTop;
+
+    if(sPos < headerHeight){
+        header.classList.remove("active");
+    }else{
+        header.classList.add("active");
+    }
+
+    if(sPos < mainSliderHeight){
+        topBtn.style.display = "none";
+        reserveWrap.style.display = "none";
+    }else{
+        topBtn.style.display = "block";
+        reserveWrap.style.display = "block";
+    }
+});
+
+// 위로가기 버튼
+topBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    document.documentElement.scrollTo({top:0, left:0, behavior:'smooth'});
+});
+
+// 서브메뉴
